1ad33a480a4a065b707802ed26ca226d6ab9c0fe
Ghita Tudor <tfghita@luxoft.com>
2017-07-20 13:25:14 +0300

    Finished
    Signed-off-by: Ghita Tudor <tfghita@luxoft.com>

--- ex21.c
+++ ex21.c
@@ -1,39 +1,44 @@
-/* 
-* NOT FINISHED
-*/
-
 #include <stdio.h>
 #include <unistd.h>
 #include <float.h>
 
-#define LOWEST_INCREMENT 1.0/32
-
 int main()
 {
-	int i = 0x7F7FFFFF;//0x3F400000;
+	unsigned int ui;
+
+	char c;
+	unsigned char uc;
+	long long int li;
+
+	int i = 0x7F7FFFFF; // setting sign bit to 0 and leaving the last bit of the exponent to 0, otherwise it would be a NAN
+
 	float *fl = (float*)&i;
+	printf("Highest float: %f\n", *fl);
 
-	printf("%f\n", *fl);
+	i |= 0x80000000;
+	printf("Lowest float: %f\n", *fl); // setting sign bit to 1 for negative numbers
 
-	return 0;
+	li = 0x7FEFFFFFFFFFFFFF; // same thing as with i, only this time on 64 bits
+	double *d = (double*)&li;
+	printf("Highest double: %f\n", *d);
 
-	float f = FLT_MAX;
-	float increment = (1 << 30) * 1024.0;
-	
-	while (increment > LOWEST_INCREMENT)
-	{
-		while (f < FLT_MAX)
-		{
-			f += increment;
-		}
-		printf("Overflow: %f\n", f);
+	li |= 0x8000000000000000; // same thing as with the float, only this time on 64 bits
+	printf("Lowest double: %f\n", *d);
 
-		f -= increment;
+	i = 0x7FFFFFFF;
+	printf("Highest signed integer: %d\n", i);
+	printf("Lowest signed integer: %d\n", i + 1);
 
-		printf("Highest point for increment %f: %f\n", increment, f);
+	ui = 0xFFFFFFFF;
+	printf("Highest unsigned integer: %u\n", ui);
 
-		increment /= 2;
-	}
+	c = 0x7F;
+
+	printf("Highest signed char: %d\n", c);
+	printf("Lowest signed char: %d\n", (char)(c + 1));
+
+	uc = 0xFF;
+	printf("Highest unsigned char: %d\n", uc);
 
 	return 0;
 }
