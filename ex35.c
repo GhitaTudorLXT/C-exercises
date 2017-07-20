ee291d52aa22193962b1ec7b79395e5f92cd4a5e
Ghita Tudor <tfghita@luxoft.com>
2017-07-20 14:47:56 +0300

    Base checking added. Also included functionality for base 1
    Signed-off-by: Ghita Tudor <tfghita@luxoft.com>

--- ex35.c
+++ ex35.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define ONE 49
 
 char digit_to_alphanum(int x)
 {
@@ -13,6 +16,14 @@ void itob(int n, char *s, int base)
 	int len = 0;
 	int i;
 
+	if (base == 1)
+	{
+		memset(s, ONE, n);
+		s[n] = '\0';
+
+		return;
+	}
+
 	while (n / base)
 	{
 		s[len++] = digit_to_alphanum(n % base);
@@ -22,7 +33,7 @@ void itob(int n, char *s, int base)
 
 	s[len++] = digit_to_alphanum(n);
 
-	s[len] = 0;
+	s[len] = '\0';
 
 	for (i = 0; i < len / 2; i++)
 	{
@@ -35,9 +46,24 @@ void itob(int n, char *s, int base)
 int main(int argc, char **argv)
 {
 	if (argc < 3)
+	{
+		printf("Usage: ./ex35 [number] [base]\n");
+
 		return 0;
+	}
+
+	int number, base;
 
 	char s[128];
+
+	number = atoi(argv[1]);
+	base = atoi(argv[2]);
+
+	if (base < 1 || base > 36)
+	{
+		printf("[base] should be in range [1, 36]\n");
+		return 0;
+	}
 
 	itob(atoi(argv[1]), s, atoi(argv[2]));
 
