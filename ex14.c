962c3892a6f5120317309e0fd67a9f833775def7
Ghita Tudor <tfghita@luxoft.com>
2017-07-20 15:52:41 +0300

    Fixed headers
    Signed-off-by: Ghita Tudor <tfghita@luxoft.com>

--- ex14.c
+++ ex14.c
@@ -9,7 +9,7 @@ int main()
   step = 20;      /* step size */ 
   celsius = lower; 
   printf("\nConversion table\n\n");
-  printf("Fahrenheit Celsius\n");
+  printf("Celsius Fahrenheit\n");
   while (celsius <= upper) { 
     fahr = (9.0 * celsius) / 5.0 + 32.0;
     printf("%3.0f \t %6.1f\n", celsius, fahr); 
