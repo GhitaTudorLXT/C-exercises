6c771a71a3d73fd589b7956e34815228d3eb068d
Ghita Tudor <tfghita@luxoft.com>
2017-07-20 14:35:32 +0300

    Much better, indeed
    Signed-off-by: Ghita Tudor <tfghita@luxoft.com>

--- ex210.c
+++ ex210.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int lower (int c)
 {
 	return c >= 'A' && c <= 'Z' ? c + ('a' - 'A') : c;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("%d\n", lower(65));
+	if (argc < 2)
+	{
+		printf("Usage: ./ex210 [string]\n");
+		return 0;
+	}
+
+	int i;
+
+	for (i = 0; i < strlen(argv[1]); i++)
+	{
+		argv[1][i] = lower(argv[1][i]);
+	}
+
+	printf("%s\n", argv[1]);
 
 	return 0;
 }
