512cc25f951b38cb753d93c0d07bde0941c7a73f
Ghita Tudor <tfghita@luxoft.com>
2017-07-20 13:24:31 +0300

    Run-time measurement done
    Signed-off-by: Ghita Tudor <tfghita@luxoft.com>

--- ex31.c
+++ ex31.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 int binsearch(int x, int *v, int len)
 {
@@ -27,13 +29,13 @@ int binsearch(int x, int *v, int len)
 	return -1;
 }
 
-/*
-int binsearch(int x, int v[], int n) 
+
+int sbinsearch(int x, int v[], int n) 
 { 
    int low, high, mid; 
    low = 0; 
    high = n - 1; 
-   while (low <= high) { 
+   while (low <= high) {
        mid = (low+high)/2; 
        if (x < v[mid]) 
            high = mid + 1; 
@@ -43,16 +45,22 @@ int binsearch(int x, int v[], int n)
            return mid; 
    } 
    return -1; 
-} 
-*/
+}
+
 
 int main(int argc, char**argv)
 {
 	if (argc < 3)
+	{
+		printf("\n Usage: ./ex31 [filename] [number]\n\n");
 		return 0;
+	}
 
 	int n;
 	int *v;
+	int result;
+
+	struct timeval start, stop;
 
 	FILE *f = fopen(argv[1], "r");
 
@@ -63,11 +71,17 @@ int main(int argc, char**argv)
 	for (int i = 0; i < n; i++)
 		fscanf(f, "%d", &v[i]);
 
-	clock_t start = clock();
-	printf("%d\n", binsearch(atoi(argv[2]), v, n));
-	clock_t stop = clock() - start;
+	gettimeofday(&start, NULL);
+	sbinsearch(atoi(argv[2]), v, n);
+	gettimeofday(&stop, NULL);
 
-	printf("Time: %ld\n", stop);
+	printf("Time for slow binsearch: %f miliseconds\n\n", (stop.tv_sec - start.tv_sec) * 1000 + (float)(stop.tv_usec - start.tv_usec) / 1000);
+
+	gettimeofday(&start, NULL);
+	binsearch(atoi(argv[2]), v, n);
+	gettimeofday(&stop, NULL);
+
+	printf("Time for binsearch: %f miliseconds\n", (stop.tv_sec - start.tv_sec) * 1000 + (float)(stop.tv_usec - start.tv_usec) / 1000);
 
 	return 0;
 }
